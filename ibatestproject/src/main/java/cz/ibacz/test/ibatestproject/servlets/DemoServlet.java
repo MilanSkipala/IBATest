d47733e8972a0d768736d8dbdd42172c9532b017
Milan Skipala <396251@mail.muni.cz>
2016-07-20 09:35:15 +0200

    Parameter processing added, JSTL in use

--- ibatestproject/src/main/java/cz/ibacz/test/ibatestproject/servlets/DemoServlet.java
+++ ibatestproject/src/main/java/cz/ibacz/test/ibatestproject/servlets/DemoServlet.java
@@ -6,7 +6,6 @@
 package cz.ibacz.test.ibatestproject.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,15 +16,20 @@ import javax.servlet.http.HttpServletResponse;
  * @author Milan
  */
 public class DemoServlet extends HttpServlet {
-
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter out = resp.getWriter();
-        out.println("<html><head><title>Hello IBA!</title></head>");
-        out.println("<body>");
-        out.println("<h1>Hello IBA!</h1>");
-        out.println("</body>");
-        out.println("</html>");	
+        //Try to convert parameter value to integer. If it fails (= exception is thrown), it means, that there 
+        //is no parameter or it has non-number value. If the parameter value is less than zero, it is changed to x=1
+        try {
+            Integer value = Integer.valueOf(req.getParameter("x"));
+            if (value < 0) {
+                throw new IllegalArgumentException();
+            }
+            req.getRequestDispatcher("/index.jsp").forward(req, resp);
+        } catch (NullPointerException | IllegalArgumentException e) {
+            req.getRequestDispatcher("/index.jsp?x=1").forward(req, resp);
+        }
     }
     
 }
--- ibatestproject/src/main/webapp/index.jsp
+++ ibatestproject/src/main/webapp/index.jsp
@@ -1,13 +1,16 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="f" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<!DOCTYPE html>
 
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Hello IBA!</title>
     </head>
     <body>
-        <h1>Hello World!</h1>
+        <c:forEach begin="1" end="${param.x}">
+            <h1>Hello IBA!</h1>
+        </c:forEach>
     </body>
 </html>
